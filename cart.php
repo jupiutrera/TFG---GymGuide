13a578bee57e809900af362c6c6ae190827873ee
jupiutrera <116831166+jupiutrera@users.noreply.github.com>
2024-05-20 10:19:29 +0000

    Cambios en el header generales

--- cart.php
+++ cart.php
@@ -1,39 +1,21 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- basic -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- mobile metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <!-- site metas -->
-    <title>GymGuide - Tienda</title>
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- bootstrap css -->
+    <title>GymGuide - Inicio</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- style css -->
     <link rel="stylesheet" href="css/style.css">
-    <!-- Responsive-->
     <link rel="stylesheet" href="css/responsive.css">
-    <!-- fevicon -->
     <link rel="icon" href="images/fevicon.png" type="image/gif" />
-    <!-- Scrollbar Custom CSS -->
     <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-    <!-- Tweaks for older IEs-->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-    <style>
-        .product-section {
-            margin-top: 50px;  /* Espacio superior */
-            margin-bottom: 50px;  /* Espacio inferior */
-        }
-    </style>
 </head>
 <body class="main-layout position_head">
     <header>
@@ -44,42 +26,52 @@
                         <div class="full">
                             <div class="center-desk">
                                 <div class="logo">
-                                    <a href="index.php" class="brand-logo"><img src="images/logo.png" alt="#" /><p>GymGuide</p></a>
+                                    <a href="index.php" class="brand-logo"><img src="images/logo.png" alt="#" />
+                                        <p>GymGuide</p>
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
                         <nav class="navigation navbar navbar-expand-md navbar-dark ">
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarsExample04">
                                 <ul class="navbar-nav mr-auto">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="http://gymguide.es">Home</a>
+                                        <a class="nav-link" href="index.php">Home</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="about.php">Sobre nosotros</a>
                                     </li>
-                                    <li class="nav-item dropdown active">
+                                    <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="shop.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             Tienda
                                         </a>
                                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                             <a class="dropdown-item" href="shop.php">Productos</a>
-                                            <a class="dropdown-item active" href="cart.php">Carrito</a>
+                                            <a class="dropdown-item" href="cart.php">Carrito</a>
                                         </div>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="contact.php">Contacto</a>
                                     </li>
-                                    <li class="nav-item d_none login_btn">
-                                       <a class="nav-link" href="login.php">Iniciar sesión</a>
-                                   </li>
-                                   <li class="nav-item d_none">
-                                       <a class="nav-link" href="registro.php">Registro</a>
-                                   </li>
+                                    <?php if (isset($_SESSION['user_id'])): ?>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="micuenta.php">Mi Cuenta</a>
+                                        </li>
+                                    <?php else: ?>
+                                        <li class="nav-item d_none login_btn">
+                                            <a class="nav-link" href="login.php">Iniciar sesión</a>
+                                        </li>
+                                        <li class="nav-item d_none">
+                                            <a class="nav-link" href="registro.php">Registro</a>
+                                        </li>
+                                    <?php endif; ?>
                                 </ul>
                             </div>
                         </nav>
